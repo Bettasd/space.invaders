32d3e087f5baa619221931a0cdd2c604a886c54f
Gál Bettina <118981037+Bettasd@users.noreply.github.com>
2023-04-27 12:35:23 +0200

    kk

--- si.py
+++ si.py
@@ -1,1 +1,20 @@
 import pygame
+import pygame.gfxdraw
+
+
+def main():
+    # pygame inicializálás
+    pygame.init()
+
+    # ablak megnyitás
+    window = pygame.display.set_mode((600, 600))
+    pygame.display.set_caption('példaprogram')
+
+    # színek
+    red = pygame.Color(255, 0, 0)
+    green = pygame.Color(0, 255, 0)
+    blue = pygame.Color(0, 0, 255)
+
+    x = 100
+    y = 100
+    r = 50
